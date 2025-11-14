5b0a2d682be9dfc230912d461c8ffcca2f449d9f
Maël Nison <nison.mael@gmail.com>
2025-11-15 00:55:38 +0100

    Fixes

--- packages/zpm-parsers/build.rs
+++ packages/zpm-parsers/build.rs
@@ -1,7 +1,11 @@
 fn main() {
     println!("cargo:rustc-check-cfg=cfg(sonic_rs)");
 
-    if cfg!(not(target_pointer_width = "32")) {
+    let target_pointer_width
+        = std::env::var("CARGO_CFG_TARGET_POINTER_WIDTH")
+            .expect("CARGO_CFG_TARGET_POINTER_WIDTH not set");
+
+    if target_pointer_width != "32" {
         println!("cargo:rustc-cfg=sonic_rs");
     }
 }
