ab8a3ee9da3ebbd554a8df05beb7397e22829287
Maël Nison <nison.mael@gmail.com>
2025-12-15 23:46:12 +0100

    Implements "yarn config" (#148)

--- packages/zpm/src/commands/config.rs
+++ packages/zpm/src/commands/config.rs
@@ -3,8 +3,6 @@ use clipanion::cli;
 use crate::{error::Error, project::Project};
 
 /// List the project's configuration values
-///
-/// [unimplemented]
 #[cli::command]
 #[cli::path("config")]
 #[cli::path("config", "get")]
@@ -17,37 +15,10 @@ impl Config {
         let project
             = Project::new(None).await?;
 
-        // let project_settings
-        //     = project.config.settings.to_btree_map();
+        let tree
+            = project.config.tree_node();
 
-        // let mut nodes = vec![];
-
-        // let value_header
-        //     = "Value:".bold().to_string();
-
-        // for (key, value) in project_settings {
-        //     let camel_key
-        //         = key.to_case(Case::Camel)
-        //             .truecolor(153, 204, 255)
-        //             .to_string();
-
-        //     let value_node = Node {
-        //         label: format!("{} {}", value_header, value.to_print_string()),
-        //         children: vec![],
-        //     };
-
-        //     nodes.push(Node {
-        //         label: camel_key,
-        //         children: vec![value_node],
-        //     });
-        // }
-
-        // let tree = Node {
-        //     label: "".to_string(),
-        //     children: nodes,
-        // };
-
-        // print!("{}", tree.to_string());
+        print!("{}", tree.to_string());
 
         Ok(())
     }
