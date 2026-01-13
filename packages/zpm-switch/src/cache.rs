7e31cfb296ec2eaa8b25aa66e684f04a7f304897
Maël Nison <nison.mael@gmail.com>
2026-01-13 14:12:33 +0100

    Update version check for older RC versions

--- packages/zpm-switch/src/cache.rs
+++ packages/zpm-switch/src/cache.rs
@@ -2,7 +2,7 @@ use std::{future::Future, io::Write};
 
 use serde::{Deserialize, Serialize};
 use zpm_parsers::JsonDocument;
-use zpm_semver::VersionRc;
+use zpm_semver::{Version, VersionRc};
 use zpm_utils::{DataType, Hash64, Path, ToFileString, ToHumanString, Unit, is_terminal};
 
 use crate::errors::Error;
@@ -20,9 +20,9 @@ pub struct CacheKey {
 impl CacheKey {
     pub fn to_npm_url(&self) -> Option<String> {
         if self.version.rc.as_ref().map_or(true, |rc| !rc.starts_with(&[VersionRc::String("git".to_string())])) {
-            // Older RC versions (<6.0.0-rc.8) are not available in npm
+            // Older RC versions (<6.0.0-rc.9) are not available in npm
             let is_older_rc_version =
-                self.version.rc.is_some() && self.version.minor <= 8;
+                self.version < Version::from_str("6.0.0-rc.9").unwrap();
 
             if self.version.major >= 6 && !is_older_rc_version {
                 return Some(format!("https://registry.npmjs.org/@yarnpkg/yarn-{}/-/yarn-{}-{}.tgz", self.platform, self.platform, self.version.to_file_string()));
